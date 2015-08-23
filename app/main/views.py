15d8ac8afb1a42397e7dd07d9b7d769d6c73d58b
jeffthemaximum <frey.maxim@gmail.com>
2015-08-23 12:06:58 -0400

    first commit

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,11 @@
 from flask import render_template
+from flask.ext.login import current_user
 from . import main
 
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    if current_user.is_authenticated():
+        return render_template('pd_list.html')
+    else:
+        return render_template('index.html')
