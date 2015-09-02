88ffdbff275649564e17e0ec5a3f3de904c1056b
jeffthemaximum <frey.maxim@gmail.com>
2015-09-02 13:21:44 -0400

    no poi on map excepts parks

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,9 +1,26 @@
+var map;
 
-    var map;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.754, lng: -73.984},
-        zoom: 12
-      });
-    }
+var noPoi = [
+{
+    featureType: "poi",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },
+  {
+    featureType: "poi.park",
+    stylers: [
+      { visibility: "on" }
+    ]
+  }
+];
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.754, lng: -73.984},
+    zoom: 12
+  });
+  map.setOptions({styles: noPoi});
+}
+
 
