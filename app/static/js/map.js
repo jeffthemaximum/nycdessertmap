4cc3be765c2c3f19c0f74466fb537789f1e01907
jeffthemaximum <frey.maxim@gmail.com>
2015-09-02 13:02:35 -0400

    first map on site

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -2,8 +2,8 @@
     var map;
     function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: 40.754, lng: -73.984},
+        zoom: 12
       });
     }
 
